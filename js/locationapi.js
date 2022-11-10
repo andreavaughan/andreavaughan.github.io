7948f2ddf5d6ff10305a833fb11e77e6e9460a23
andreavaughan <andrea.vaughan@pendo.io>
2022-11-10 16:02:29 -0500

    more updates to save

---
+++ js/locationapi.js
@@ -0,0 +1,16 @@
+// setTimeout(() => {
+//     if (pendo.isReady()){
+//         pendo.location.setUrl('http://localhost:8000/locationapi.html/newshiny'); 
+//         console.log(pendo.location.getHref());
+//     }
+
+// }, 5000)
+
+
+
+// function changeLocation(){
+//     if (pendo.isReady()){
+//         pendo.location.setUrl('http://localhost:8000/locationapi.html/newshiny'); 
+//         console.log(pendo.location.getHref());
+//     }
+// }
