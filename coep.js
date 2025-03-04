4a94a5696b73af77951b9d66c3a326ee52e8a33f
andreavaughan <andrea.vaughan@pendo.io>
2025-03-04 10:39:29 -0500

    changed corp header

--- coep.js
+++ coep.js
@@ -38,7 +38,7 @@ if(typeof window === 'undefined') {
       const headers = new Headers(r.headers);
       headers.set("Cross-Origin-Embedder-Policy", "credentialless"); // or: require-corp
       headers.set("Cross-Origin-Opener-Policy", "same-origin");
-      headers.set("Cross-Origin-Resource-Policy", "same-site");
+      headers.set("Cross-Origin-Resource-Policy", "cross-origin");
       
       return new Response(r.body, { status: r.status, statusText: r.statusText, headers });
     }
